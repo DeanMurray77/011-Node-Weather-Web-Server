4c941e36ce0c411e23398d4b23b7020124b5481b
DeanMurray77 <35736560+DeanMurray77@users.noreply.github.com>
2019-06-29 07:55:21 -0600

    Delete config.js
    Never should have been here

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    //DarkSky API
-    url: "https://api.darksky.net/forecast/9f4aa29a4a1f41f68c39112739d8c925/",
-    mapBoxURL: "https://api.mapbox.com/geocoding/v5/mapbox.places/",
-    mapBoxAPIKey: "?access_token=pk.eyJ1IjoiZGVhbm11cnJheSIsImEiOiJjanY3OTM0MWowNHY0NGVudGVwdmY4MGRqIn0.KlMxE7Z-KiK_fJ9rR3N-BA&limit=1"
-}
-
-module.exports = config; 
